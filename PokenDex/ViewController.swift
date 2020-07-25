b7f24c91b612c832d1c74df415c455e4346fb28a
originpulses <s3528350@student.rmit.edu.au>
2020-07-25 12:41:49 +1000

    initial commit

---
+++ PokenDex/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  PokenDex
+//
+//  Created by Sabih on 25/7/20.
+//  Copyright © 2020 RMIT. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var blueButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
