5415e552f6ec769493334b26b4f6e4d053e03915
originpulses <s3528350@student.rmit.edu.au>
2020-08-20 12:23:08 +1000

    added and customised nabber

---
+++ PokenDex/Controller/PokedexController.swift
@@ -0,0 +1,19 @@
+//
+//  PokedexController.swift
+//  PokenDex
+//
+//  Created by Sabih on 20/8/20.
+//  Copyright © 2020 RMIT. All rights reserved.
+//
+
+import UIKit
+
+private let reuseIdentifier = "PokeCell"
+
+class PokedexController: UICollectionViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
