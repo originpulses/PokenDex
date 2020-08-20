f11e72ba59dd35ac1e4c59f42029f100ce059b39
originpulses <s3528350@student.rmit.edu.au>
2020-08-21 00:01:35 +1000

    setting up detail view

--- PokenDex/ViewModel/PokedexController.swift
+++ PokenDex/ViewModel/PokedexController.swift
@@ -12,21 +12,30 @@ private let reuseIdentifier = "PokeCell"
 
 class PokedexController: UICollectionViewController {
     
+    private let viewModel = PokemonViewModel()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
     }
-}
-
-// Extension for Controlling PokeCells
-extension PokedexController {
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let cell = sender as? UICollectionViewCell, let indexPath = self.collectionView?.indexPath(for: cell) else {return}
+        if let destination = segue.destination as? PokedexController {
+            let selectedPokemon = viewModel.getPokemon(byIndex: indexPath.row)
+            destination.selectedPokemon = selectedPokemon
+        }
+    }
     
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 6
+        
+        return viewModel.count
     }
     
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath)
-        return cell
+        
+        let imageView = cell.viewWithTag(1000) as? UIImageView
+        let pokemonTitle = cell.viewWithTag(1001) as? UILabel
     }
 }
