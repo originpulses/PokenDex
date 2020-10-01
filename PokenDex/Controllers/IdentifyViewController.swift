e5ff97c8a782afff1fb33785e1f2d1533952cc60
originpulses <s3528350@student.rmit.edu.au>
2020-10-01 16:54:01 +1000

    added in coreml model

--- PokenDex/Controllers/IdentifyViewController.swift
+++ PokenDex/Controllers/IdentifyViewController.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import CoreML
+import Vision
 
 class IdentifyViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
@@ -22,18 +24,46 @@ class IdentifyViewController: UIViewController, UIImagePickerControllerDelegate,
         imagePicker.sourceType = .photoLibrary
     }
     
-    func imagePickerController(_ picker: UIImagePickerController,
-                               didFinishPickingMediaWithInfo info: [String : Any]) {
-        let userPickedImage = info[UIImagePickerControllerEditedImage]
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
         
-        imageView.image = userPickedImage as? UIImage
+        if let userPickedImage = info[UIImagePickerControllerEditedImage] as? UIImage {
+        
+            guard let ciImage = CIImage(image: userPickedImage) else {
+                fatalError("cannot get image data")
+            }
+            
+            detect(image: ciImage)
+            imageView.image = userPickedImage
+            
+        }
+        
         imagePicker.dismiss(animated: true, completion: nil)
+    }
+    
+    func detect(image: CIImage) {
+        
+        guard let model = try? VNCoreMLModel(for: PokemonImageClassifier().model) else {
+            
+            fatalError("cannot import model")
+        }
+        
+        let request = VNCoreMLRequest(model: model) { (request, error) in
+            let classification = request.results?.first as? VNClassificationObservation
+            
+            self.navigationItem.title = classification?.identifier
+        }
+        
+        let handler = VNImageRequestHandler(ciImage: image)
+        
+        do {
+            try handler.perform([request])
+        }
+        catch { print(error) }
+        
     }
     
     @IBAction func cameraPressed(_ sender: UIBarButtonItem) {
         present(imagePicker, animated: true, completion: nil)
     }
-    
-    
     
 }
